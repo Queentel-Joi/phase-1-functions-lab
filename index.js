f05186fc618b4acada9fabe125d3e28ce3d3b7f5
Queentel <joiqueentel@gmail.com>
2025-06-10 23:18:52 +0300

    creates function

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Code your solution in this file!
+const hqStreet = 42
+
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(pickupLocation - hqStreet);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
+
+module.exports = {
+  distanceFromHqInBlocks,
+  distanceFromHqInFeet,
+  distanceTravelledInFeet,
+  calculatesFarePrice
+};
+
+function calculatesFarePrice(start, destination) {
+  const distance = Math.abs(destination - start) * 264;
+
+  if (distance <= 400) {
+
+    return 0;
+  } else if (distance > 400 && distance <= 2000) 
+
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+
+    return 25;
+  } else {
+
+    return 'cannot travel that far';
+  }
+
+
+
